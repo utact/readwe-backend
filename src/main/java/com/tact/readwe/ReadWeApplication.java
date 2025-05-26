27a88491ba863ed37a206cc1b6a525153d2a0bd4
Yoo Seung-jun <funczun@gmail.com>
2025-05-26 10:49:37 +0900

    chore: initial project setup

---
+++ src/main/java/com/tact/readwe/ReadWeApplication.java
@@ -0,0 +1,13 @@
+package com.tact.readwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadWeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadWeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tact/readwe/ReadWeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tact.readwe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadWeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
