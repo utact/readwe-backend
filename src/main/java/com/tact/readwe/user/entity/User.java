0bc753b818a7e0cce88f0d64d5e7819bf32a4589
Yoo Seung-jun <funczun@gmail.com>
2025-05-28 09:29:56 +0900

    feat: add User entity

---
+++ src/main/java/com/tact/readwe/user/entity/User.java
@@ -0,0 +1,47 @@
+package com.tact.readwe.user.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table
+public class User {
+    @Id
+    @Column(name = "user_id", length = 36, nullable = false)
+    private String userId;
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "password", length = 100)
+    private String password;
+
+    @Column(name = "color_code", length = 7)
+    private String colorCode;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public User() {}
+
+    private User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static User signUpOf(String name, String email, String password) {
+        return new User(name, email, password);
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.userId = UUID.randomUUID().toString();
+        this.createdAt = LocalDateTime.now();
+    }
+}
