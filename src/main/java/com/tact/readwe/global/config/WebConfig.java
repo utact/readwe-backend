8ab48a1bfecbe5949e0323758994d5284033ef58
Yoo Seung-jun <funczun@gmail.com>
2025-06-04 14:42:09 +0900

    refactor: move configs to global.config package

--- src/main/java/com/tact/readwe/global/config/WebConfig.java
+++ src/main/java/com/tact/readwe/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.tact.readwe.config;
+package com.tact.readwe.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
