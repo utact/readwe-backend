9bdd1dad989f3f6354e056c2397fffe2659a33e9
Yoo Seung-jun <funczun@gmail.com>
2025-06-09 13:55:47 +0900

    refactor: add NonNull annotation to clarify method contract

--- src/main/java/com/tact/readwe/global/auditing/AuditorAwareImpl.java
+++ src/main/java/com/tact/readwe/global/auditing/AuditorAwareImpl.java
@@ -3,10 +3,12 @@ package com.tact.readwe.global.auditing;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import reactor.util.annotation.NonNull;
 
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
+    @NonNull
     @Override
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
